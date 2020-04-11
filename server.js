18cfc0e6f178434b1cf9f63a84b2d9f42c687a90
longwMJ <3029043191@qq.com>
2020-04-11 08:19:38 +0800

    添加服务

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+const express = require('express');
+const expressGraphql = require('express-graphql');
+const app = express();
+
+const schema = require('./schema');
+app.use('/graphql', expressGraphql({
+    schema,
+    graphiql: true
+}));
+
+app.get('/', (req, res) => res.end('index'));
+
+app.listen(8000, (err) => {
+    if (err) { throw new Error(err); }
+    console.log('*** server started ***');
+});
+// 运行一下node server，并打开http://localhost:8000/
+
+// http://localhost:8000/graphql
